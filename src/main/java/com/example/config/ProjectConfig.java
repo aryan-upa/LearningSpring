7284cac156b79c49ddadb270d6361f831ca89d0d
Aryan Upadhyay <aryan.offc@gmail.com>
2022-06-27 10:07:06 +0530

    Creating beans using @Bean Annotation.

---
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,41 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+    @Configuration This annotation is part of the spring core framework. This annotation indicates that the class has @Bean
+                    definition methods. So Spring can process the class and generate Spring Beans to be used in the
+                    application.
+ */
+@Configuration
+public class ProjectConfig {
+
+    /**
+        @Bean annotation which lets Spring know that it needs to call this method when it initializes its context and
+              adds the returned value to the context.
+     */
+    @Bean
+    Vehicle vehicle() {
+        var vehicle  = new Vehicle();
+        vehicle.setName("BMW Urus");
+        return vehicle;
+    }
+
+    /**
+        hello method is used to return the "hello world" string to the caller.
+        The method names usually follow verbs notation. But, for methods which we will use to create beans, can use
+        nouns as names. This will be a good practice as the method names will become bean names as well in the context.
+     */
+    @Bean
+    String hello() {
+        return "Hello World";
+    }
+
+
+    @Bean
+    Integer number() {
+        return 16;
+    }
+}
---
+++ src/main/java/com/example/main/Main.java
@@ -0,0 +1,39 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from non-spring context is : " + vehicle.getName());
+
+        /*
+            With this line we are getting the context of this project as defined in the ProjectConfig class.
+            AnnotationConfigApplicationContext is an implementation of the ApplicationContext Interface which helps us
+                in creating beans in the system.
+
+            This way spring will add all the beans in the Spring IOC Container context created.
+         */
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle vehicle1 = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from the Spring Context is : " + vehicle1.getName());
+
+        /*
+            We do not need to do any explicit casting while fetching a bean from context. Spring is smart enough to look
+             for a bean of the type you requested in its context. If such a bean does not exist, Spring will throw an
+            exception.
+         */
+        var hello = context.getBean(String.class);
+        System.out.println("Value obtained from the string context is : " + hello);
+        var number = context.getBean(Integer.class);
+        System.out.println("Value obtained from the string context is : " + number);
+
+    }
+
+}

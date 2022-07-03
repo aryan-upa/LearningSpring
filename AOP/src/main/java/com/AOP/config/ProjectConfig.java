6790ae2be56e0d23a0772baf4cacec836864bd08
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-03 19:38:04 +0530

    Enabling Aspect in ProjectConfig.

--- AOP/src/main/java/com/AOP/config/ProjectConfig.java
+++ AOP/src/main/java/com/AOP/config/ProjectConfig.java
@@ -2,9 +2,11 @@ package com.AOP.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration()
 @ComponentScan(basePackages = "com.AOP.beans")
+@EnableAspectJAutoProxy
 public class ProjectConfig {
     // Placeholder Class.
 }

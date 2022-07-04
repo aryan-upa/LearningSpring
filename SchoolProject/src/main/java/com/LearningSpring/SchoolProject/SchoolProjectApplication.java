fe69d5ba71014582a5458ef8f18fb1af784784b1
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-04 23:09:09 +0530

    About how to debug SpringBoot application's internal configuration. About excluding files from Application.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/SchoolProjectApplication.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/SchoolProjectApplication.java
@@ -3,6 +3,22 @@ package com.LearningSpring.SchoolProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	For excluding any class from automatically generating beans by the Spring framework. We can use the "exclude list"
+	to remove such classes.
+
+	@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceProperties.class })
+	in such format.
+
+	@SpringBootApplication annotation is made up of several inner annotations.
+		-- @SpringBootConfiguration : It is same as @Configuration annotation in the earlier projects.
+		-- @SpringAutoConfiguration : This annotation is responsible for autoconfiguring all the required files in our
+									  System & which are defined in the application.properties file.
+		-- @ComponentScan			: This annotation is required for the Spring framework to determine where to look
+		   							  for classes to create beans.
+		-- etc.
+ */
+
 @SpringBootApplication
 public class SchoolProjectApplication {
 

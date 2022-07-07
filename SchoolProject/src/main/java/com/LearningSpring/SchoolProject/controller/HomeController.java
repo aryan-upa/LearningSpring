e427c5e714b1a9259293b2bc08d75fb1ed2b210c
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-07 19:23:54 +0530

    Changes HomeController.java and added template file in templates/home.html

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/HomeController.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.LearningSpring.SchoolProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    /* We can have multiple mappings in one @RequestMapping */
-    @RequestMapping(value={"home","","/"})
-    public String displayHomePage() {
+    /* We can have multiple mappings in one @RequestMapping
+    *  Model is an interface which acts as a container to carry data from backend to frontend.
+    * */
+    @RequestMapping(value={"/home", "", "/"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Aryan Upadhyay");
         return "home.html";
     }
 

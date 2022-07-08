6bf021569a55977f488f5dfed5b14e4c58f826cd
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-08 14:00:22 +0530

    Implemented lombok in model files.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/model/Holiday.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/model/Holiday.java
@@ -1,5 +1,8 @@
 package com.LearningSpring.SchoolProject.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
 
     private final String day;
@@ -12,21 +15,4 @@ public class Holiday {
         STATE
     }
 
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
-    }
 }

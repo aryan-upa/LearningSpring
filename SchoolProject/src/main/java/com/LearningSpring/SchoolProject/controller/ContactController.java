3001730fc6df70a1cff268a7573ada889e116e5f
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-08 09:13:50 +0530

    Used Contact class to accept the message and ContactService class to perform operation on data.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/ContactController.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/ContactController.java
@@ -1,9 +1,11 @@
 package com.LearningSpring.SchoolProject.controller;
 
+import com.LearningSpring.SchoolProject.model.Contact;
+import com.LearningSpring.SchoolProject.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.logging.Logger;
@@ -12,6 +14,13 @@ import java.util.logging.Logger;
 public class ContactController {
 
     private final Logger log = Logger.getLogger(ContactController.class.getName());
+
+    private final ContactService contactService;
+
+    @Autowired
+    ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
 
     @RequestMapping("/contact")
     public String displayContactPage() {
@@ -28,20 +37,20 @@ public class ContactController {
 
         To change the parameter name we have to externally define what is the name of field defined in the form.
         saveMessage(@RequestParam(name="name") String anyOtherName)
+
+        Or we can create a POJO class to reflect all the fields in the Form and use that POJO class as reference to what
+         we have to input.
+
+        Usually in Production Grade applications we do not do much with data except preliminary validations inside the
+        controller class. Then we send our data to the Service layer for further processing & use.
      */
     @PostMapping("/saveMsg") // OR @RequestMapping("/saveMsg", Method = POST)
-    public ModelAndView saveMessage(
-            @RequestParam String name,
-            @RequestParam String mobileNum,
-            @RequestParam String email,
-            @RequestParam String subject,
-            @RequestParam String message) {
-        log.info("Name : "+ name);
-        log.info("mobile number : " + mobileNum);
-        log.info("email : " + email);
-        log.info("subject : " + subject);
-        log.info("message : " + message);
-        return new ModelAndView("redirect:/contact");
+    public ModelAndView saveMessage(Contact contact) {
+        boolean isMessageSaved = contactService.saveMessageDetails(contact);
+        if (isMessageSaved)
+            return new ModelAndView("redirect:/contact?saved=true");
+        else
+            return new ModelAndView("redirect:/contact?saved=false");
     }
 
 }

c4face3671533793a5a1630f02fd5689a3fc0a35
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-10 10:01:43 +0530

    Disabled csrf security and applied permitAll to mvcMatchers patterns of the site.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/config/ProjectSecurityConfig.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/config/ProjectSecurityConfig.java
@@ -9,11 +9,19 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll().and()
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers("/dashboard").authenticated()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .and()
                 .formLogin().and()
                 .httpBasic();
     }
-    
+
 }

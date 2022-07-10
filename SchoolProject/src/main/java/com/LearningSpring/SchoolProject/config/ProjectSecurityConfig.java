088c75ea7d76c473595f6222cdc1dc620369cf75
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-10 13:00:57 +0530

    Implemented user authentication using inMemoryAuthentication.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/config/ProjectSecurityConfig.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/config/ProjectSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.LearningSpring.SchoolProject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -24,4 +26,13 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
 }

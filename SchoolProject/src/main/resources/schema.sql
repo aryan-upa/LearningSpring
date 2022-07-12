ad6092ed18933089c554e8693b304b9dc59bc2a9
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-12 23:37:04 +0530

    Added schema related to h2 Database.

---
+++ SchoolProject/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+create table if not exists `contact_msg` (
+    `contact_id` int AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(100) not null,
+    `mobile_num` varchar(100) not null,
+    `email` varchar(100) not null,
+    `subject` varchar(100) not null,
+    `message` varchar(500) not null,
+    `status` varchar(10) not null,
+    `created_at` timestamp not null,
+    `created_by` varchar(50) not null,
+    `updated_at` timestamp default null,
+    `updated_by` varchar(50) default null
+);

af84b901ec92f54ac174517b18c07d8d300b8537
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-13 23:54:50 +0530

    Created NewAgeSchoolConstants.java file.

---
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/constants/NewAgeSchoolConstants.java
@@ -0,0 +1,9 @@
+package com.LearningSpring.SchoolProject.constants;
+
+public interface NewAgeSchoolConstants {
+
+    String ANONYMOUS = "Anonymous";
+    String OPEN = "Open";
+    String CLOSE = "Close";
+
+}

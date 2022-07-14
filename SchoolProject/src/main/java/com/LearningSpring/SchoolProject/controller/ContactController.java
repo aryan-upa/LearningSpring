e8afd74d10c0b02fc3ad7a298feadf0c594a00b6
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-15 00:04:48 +0530

    added ContactController.displayMessages method.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/ContactController.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/ContactController.java
@@ -10,7 +10,9 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -59,6 +61,14 @@ public class ContactController {
 
         contactService.saveMessageDetails(contact);
         return "redirect:/contact";
+    }
+
+    @RequestMapping("/displayMessages")
+    public ModelAndView displayMessages(Model model) {
+        List<Contact> contactMsgs = contactService.findMsgsWithOpenStatus();
+        ModelAndView modelAndView = new ModelAndView("messages.html");
+        modelAndView.addObject("contactMsgs", contactMsgs);
+        return modelAndView;
     }
 
 }

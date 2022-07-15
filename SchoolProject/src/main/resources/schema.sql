82315675b2a49b65d91241c8ec165dc3220b77ca
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-15 23:45:55 +0530

    Added table for holidays list.

--- SchoolProject/src/main/resources/schema.sql
+++ SchoolProject/src/main/resources/schema.sql
@@ -11,3 +11,13 @@ create table if not exists `contact_msg` (
     `updated_at` timestamp default null,
     `updated_by` varchar(50) default null
 );
+
+create table if not exists `holidays` (
+    `day` varchar(20) not null,
+    `reason` varchar(100) not null,
+    `type` varchar(20) not null,
+    `created_at` timestamp not null,
+    `created_by` varchar(50) not null,
+    `updated_at` timestamp default null,
+    `updated_by` varchar(50) default null
+);

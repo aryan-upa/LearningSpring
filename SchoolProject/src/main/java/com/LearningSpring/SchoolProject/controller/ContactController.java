d087e8b28460d81899a99396bcad184e71f1cd0b
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-15 20:19:42 +0530

    Created method closeMsg to map with /closeMsg function.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/ContactController.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/ContactController.java
@@ -4,12 +4,11 @@ import com.LearningSpring.SchoolProject.model.Contact;
 import com.LearningSpring.SchoolProject.service.ContactService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -69,6 +68,12 @@ public class ContactController {
         ModelAndView modelAndView = new ModelAndView("messages.html");
         modelAndView.addObject("contactMsgs", contactMsgs);
         return modelAndView;
+    }
+
+    @GetMapping("/closeMsg")
+    public String closeMsg(@RequestParam int id, Authentication authentication) {
+        contactService.updateMsgStatus(id, authentication.getName());
+        return "redirect:/displayMessages";
     }
 
 }

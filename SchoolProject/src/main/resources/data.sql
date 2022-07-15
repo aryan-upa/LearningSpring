5e3588f2fbea1d1ffa4fb1c2757cd14f0cb495d5
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-15 23:46:30 +0530

    Added data in the holidays table.

---
+++ SchoolProject/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`) values ('1 Jan', 'New Year''s Day', 'FESTIVAL', CURDATE(), 'DBA');
+insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`) values ('15 Apr', 'Good Friday', 'FESTIVAL', CURDATE(), 'DBA');
+insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`) values ('26 Jan', 'Republic Day', 'CENTRAL', CURDATE(), 'DBA');
+insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`) values ('15 Aug', 'Independence Day', 'CENTRAL', CURDATE(), 'DBA');
+insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`) values ('2 Oct', 'Mahatma Gandhi''s Birthday', 'CENTRAL', CURDATE(), 'DBA');
+insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`) values ('24 Nov', 'Guru Teg Bahadur Martyrdom Day', 'STATE', CURDATE(), 'DBA');
+insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`) values ('25 Dec', 'Christmas', 'FESTIVAL', CURDATE(), 'DBA');

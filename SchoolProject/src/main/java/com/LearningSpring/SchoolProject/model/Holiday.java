159a5626947f2052d7eedf8d9947d5c875c773db
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-20 20:58:50 +0530

    Modified Holiday class to support JPA Specifications.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/model/Holiday.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/model/Holiday.java
@@ -1,12 +1,24 @@
 package com.LearningSpring.SchoolProject.model;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "holidays")
 public class Holiday extends BaseEntity {
 
+    @Id
     private String day;
     private String reason;
+
+    @Enumerated(EnumType.STRING)
     private Type type;
 
     public enum Type {
@@ -15,4 +27,16 @@ public class Holiday extends BaseEntity {
         STATE
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Holiday holiday = (Holiday) o;
+        return day != null && Objects.equals(day, holiday.day);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }

261e77d75b04268d2bd22e61fd4590c375e55db9
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-20 23:41:04 +0530

    Converted ContactRepository from class to interface and extended CrudRepository. Added findByStatus Method.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/repository/ContactRepository.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/repository/ContactRepository.java
@@ -1,57 +1,14 @@
 package com.LearningSpring.SchoolProject.repository;
 
 import com.LearningSpring.SchoolProject.model.Contact;
-import com.LearningSpring.SchoolProject.rowMappers.ContactRowMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public class ContactRepository {
+public interface ContactRepository extends CrudRepository<Contact, Integer> {
 
-    private final JdbcTemplate jdbcTemplate;
+    List<Contact> findByStatus(String status);
 
-    @Autowired
-    public ContactRepository(JdbcTemplate template) {
-        this.jdbcTemplate = template;
-    }
-
-    public int saveContactMsg(Contact contact) {
-        String sql = "insert into contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by) values(?,?,?,?,?,?,?,?);";
-        return jdbcTemplate.update(
-                sql,
-                contact.getName(),
-                contact.getMobileNum(),
-                contact.getEmail(),
-                contact.getSubject(),
-                contact.getMessage(),
-                contact.getStatus(),
-                contact.getCreatedAt(),
-                contact.getCreatedBy());
-    }
-
-    public List<Contact> findMsgsWithStatus(String status) {
-        String sql = "select * from contact_msg where status = ?";
-        return jdbcTemplate.query(sql, (PreparedStatementSetter) ps -> ps.setString(1, status), new ContactRowMapper());
-    }
-
-    public int updateMsgStatus(int contactID, String close, String updatedBy) {
-        String sql = "update contact_msg set status = ?, updated_by = ?, updated_at = ? where contact_id = ?";
-        return jdbcTemplate.update(sql, new PreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps) throws SQLException {
-                ps.setString(1, close);
-                ps.setString(2, updatedBy);
-                ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
-                ps.setInt(4, contactID);
-            }
-        });
-    }
 }

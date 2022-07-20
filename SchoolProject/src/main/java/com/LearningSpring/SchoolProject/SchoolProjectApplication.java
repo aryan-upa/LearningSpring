b3ceebc1081d2a39500f9c1c96a516c37facf3bb
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-20 23:43:03 +0530

    Modified SchoolProjectApplication to use JPA.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/SchoolProjectApplication.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/SchoolProjectApplication.java
@@ -1,7 +1,10 @@
 package com.LearningSpring.SchoolProject;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /*
 	For excluding any class from automatically generating beans by the Spring framework. We can use the "exclude list"
@@ -20,6 +23,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableJpaRepositories("com.LearningSpring.SchoolProject.repository")
+@EntityScan("com.LearningSpring.SchoolProject.model")
 public class SchoolProjectApplication {
 
 	public static void main(String[] args) {

34e4aa356e3e5bdd8708fe450327ecac3661af39
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-20 23:40:35 +0530

    Converted HolidayRepository from class to interface and extended CrudRepository.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/repository/HolidayRepository.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/repository/HolidayRepository.java
@@ -1,26 +1,22 @@
 package com.LearningSpring.SchoolProject.repository;
 
 import com.LearningSpring.SchoolProject.model.Holiday;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public class HolidayRepository {
+public interface HolidayRepository extends CrudRepository<Holiday, String> {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public HolidayRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Holiday> getAllHolidays() {
-        String sql = "select * from holidays";
-        var rowMapper = BeanPropertyRowMapper.newInstance(Holiday.class);
-        return jdbcTemplate.query(sql, rowMapper);
-    }
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public HolidayRepository(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//
+//    public List<Holiday> getAllHolidays() {
+//        String sql = "select * from holidays";
+//        var rowMapper = BeanPropertyRowMapper.newInstance(Holiday.class);
+//        return jdbcTemplate.query(sql, rowMapper);
+//    }
 }

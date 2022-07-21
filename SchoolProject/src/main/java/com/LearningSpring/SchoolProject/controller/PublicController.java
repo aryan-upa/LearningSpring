452668d422aff1406a8d1ba818bc84d9ee115a5d
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-21 23:05:18 +0530

    Created PublicController class.

---
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/PublicController.java
@@ -0,0 +1,37 @@
+package com.LearningSpring.SchoolProject.controller;
+
+import com.LearningSpring.SchoolProject.model.Person;
+import com.LearningSpring.SchoolProject.service.PersonService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/public")
+public class PublicController {
+
+    @Autowired
+    PersonService personService;
+
+    @GetMapping("/register")
+    public String displayRegisterPage(Model model) {
+        model.addAttribute("person", new Person());
+        return "register.html";
+    }
+
+    @PostMapping("/createUser")
+    public String createUser(@Valid @ModelAttribute("person") Person person, Errors errors) {
+        if(errors.hasErrors())
+            return "register.html";
+        return "redirect:/login?register=true";
+    }
+
+}

de21e3f81a429857d66a28a71a4691ae4f1344a7
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-21 10:00:48 +0530

    enabled auditing support.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/SchoolProjectApplication.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/SchoolProjectApplication.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /*
@@ -25,6 +26,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories("com.LearningSpring.SchoolProject.repository")
 @EntityScan("com.LearningSpring.SchoolProject.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class SchoolProjectApplication {
 
 	public static void main(String[] args) {

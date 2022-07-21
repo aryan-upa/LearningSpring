b7e69b6ac43dd9c4d93371f8e478da4ce466a256
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-21 23:07:47 +0530

    Modified LoginController to accept registered query.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/LoginController.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/LoginController.java
@@ -20,12 +20,16 @@ public class LoginController {
     public String displayLoginPage(
             @RequestParam(value = "error", required = false) String error,
             @RequestParam(value = "logout", required = false) String logout,
+            @RequestParam(value = "register", required = false) String register,
             Model model) {
         String errorMessage = null;
         if(error != null)
             errorMessage = "Username or Password is incorrect !!";
         else if(logout != null)
             errorMessage = "You have been successfully logged out!!";
+        else if(register != null)
+            errorMessage = "You have been successfully register, log in with required credentials.";
+
         model.addAttribute("errorMessage", errorMessage);
         return "login.html";
     }

f67db02be2b5acc81c3fecbdfe2201f3963a28e8
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-23 20:26:05 +0530

    added createNewPerson method.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/service/PersonService.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/service/PersonService.java
@@ -1,8 +1,32 @@
 package com.LearningSpring.SchoolProject.service;
 
+import com.LearningSpring.SchoolProject.constants.NewAgeSchoolConstants;
+import com.LearningSpring.SchoolProject.model.Person;
+import com.LearningSpring.SchoolProject.model.Roles;
+import com.LearningSpring.SchoolProject.repository.PersonRepository;
+import com.LearningSpring.SchoolProject.repository.RolesRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
+    private final PersonRepository personRepository;
+    private final RolesRepository rolesRepository;
+
+    public PersonService(PersonRepository personRepository, RolesRepository rolesRepository) {
+        this.personRepository = personRepository;
+        this.rolesRepository = rolesRepository;
+    }
+
+    public boolean createNewPerson(Person person) {
+        boolean isSaved = false;
+
+        Roles role = rolesRepository.getByRoleName(NewAgeSchoolConstants.STUDENT_ROLE);
+        person.setRoles(role);
+
+        person = personRepository.save(person);
+        if(person.getPersonId() > 0)
+            isSaved = true;
+        return isSaved;
+    }
 }

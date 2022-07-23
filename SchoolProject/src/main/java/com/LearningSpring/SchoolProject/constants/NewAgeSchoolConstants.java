189c38ece0c9b6fd40b54053841f0631866cd7fc
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-23 20:25:03 +0530

    Added new Role constants.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/constants/NewAgeSchoolConstants.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/constants/NewAgeSchoolConstants.java
@@ -5,5 +5,8 @@ public interface NewAgeSchoolConstants {
     String ANONYMOUS = "Anonymous";
     String OPEN = "Open";
     String CLOSE = "Close";
+    String STUDENT_ROLE = "STUDENT";
+
+    String ADMIN_ROLE = "ADMIN";
 
 }

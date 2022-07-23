2e1e0c7c3d134221ac1e4f300e70376904eaacf0
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-23 20:26:59 +0530

    added getByRoleName method.

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/repository/RolesRepository.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/repository/RolesRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RolesRepository extends JpaRepository<Roles, Integer> {
 
+    Roles getByRoleName(String roleName);
 }

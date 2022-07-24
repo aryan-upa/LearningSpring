d605bc308cd1020ecd7987e2ae5f953cc82e9c91
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-24 09:06:15 +0530

    created SchoolAuthenticationProvider class.

---
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/security/NewAgeSchoolAuthenticationProvider.java
@@ -0,0 +1,53 @@
+package com.LearningSpring.SchoolProject.security;
+
+import com.LearningSpring.SchoolProject.model.Person;
+import com.LearningSpring.SchoolProject.model.Roles;
+import com.LearningSpring.SchoolProject.repository.PersonRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class NewAgeSchoolAuthenticationProvider implements AuthenticationProvider {
+
+    private final PersonRepository personRepository;
+
+    public NewAgeSchoolAuthenticationProvider(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        Person person = personRepository.readByEmail(username);
+        if (person == null)
+            throw new BadCredentialsException("user does not exist.");
+        if (person.getPersonId() < 0)
+            throw new BadCredentialsException("Invalid User");
+        if(!person.getPwd().equals(pwd))
+            throw new BadCredentialsException("Invalid UserID or Password!");
+        return new UsernamePasswordAuthenticationToken(
+                person.getName(), pwd, getGrantedAuthorities(person.getRoles())
+        );
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Roles roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + roles.getRoleName()));
+        return authorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}

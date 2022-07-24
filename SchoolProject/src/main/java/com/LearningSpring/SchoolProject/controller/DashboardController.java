a415c0492d52943c2a7528926007886d58dbba27
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-24 23:54:40 +0530

    Modified DashboardController class to display dashboard with loggedInPerson

--- SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/DashboardController.java
+++ SchoolProject/src/main/java/com/LearningSpring/SchoolProject/controller/DashboardController.java
@@ -1,6 +1,10 @@
 package com.LearningSpring.SchoolProject.controller;
 
+import com.LearningSpring.SchoolProject.model.Person;
+import com.LearningSpring.SchoolProject.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DashboardController {
 
+    final PersonRepository personRepository;
+
+    public DashboardController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model, Authentication authentication) {
-        model.addAttribute("username", authentication.getName());
+    public String displayDashboard(Model model, Authentication authentication, HttpSession session) {
+        Person person = personRepository.readByEmail(authentication.getName());
+        session.setAttribute("loggedInPerson", person);
+        model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
         return "dashboard.html";
     }
